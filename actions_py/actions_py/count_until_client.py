23b9847dcab8f07e1ae0e181cce8cf63b90f74dc
ThiwankaReiss <thiwankar2003@gmail.com>
2026-01-09 22:48:22 +0530

    Created action client

---
+++ actions_py/actions_py/count_until_client.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionClient
+from rclpy.action.client import ClientGoalHandle
+from my_robot_interfaces.action import CountUntil
+
+
+class CountUntilClientNode(Node): # MODIFY NAME
+    def __init__(self):
+        super().__init__("count_until_client") # MODIFY NAME
+        self.count_until_client = ActionClient(self, CountUntil, "count_until")
+
+    def send_goal(self, target_number, period):
+        #Wait for the action server
+        self.count_until_client.wait_for_server()
+
+        # Create a goal
+        goal = CountUntil.Goal()
+        goal.target_number = target_number
+        goal.period = period
+
+        #send goal
+        self.get_logger().info("Sending goal")
+        self.count_until_client.send_goal_async(goal).\
+            add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self , future):
+        self.goal_handle_:ClientGoalHandle = future.result()
+        if self.goal_handle_.accepted:
+            self.goal_handle_.get_result_async().add_done_callback(self.goal_result_callback)
+
+    def goal_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info("Result: "+str(result.reached_number))
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CountUntilClientNode() # MODIFY NAME
+    node.send_goal(6,1.0)
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
